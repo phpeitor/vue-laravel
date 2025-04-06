3a434854eb77e72a748ae592c4861ef592e70e0a
FORTEL\alejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-04-06 01:18:39 -0500

    models kit commit

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Classes extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function sections()
+    {
+        return $this->hasMany(Section::class, 'class_id');
+    }
 }
